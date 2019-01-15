0e8a83516839f521f4288ae407c78109a9057b60
duka <duka@outlook.com>
2019-01-15 22:46:50 +0100

    Setup

---
+++ src/router.js
@@ -0,0 +1,29 @@
+/* tslint:disable:no-var-requires */
+
+import Vue from 'vue'
+import Router from 'vue-router'
+import Master from './views/layout/Master'
+import Home from './views/Home'
+// import {Master, Login, List, Inbox, Profile} from './views'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Master,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: Home,
+        },
+      ]
+    }
+  ],
+  linkActiveClass: 'is-active',
+})
+
+export default router
---
+++ src/views/layout/Master.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header-component />
+    <router-view />
+    <footer-component />
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from './../../components/layout/Header'
+  import FooterComponent from './../../components/layout/Footer'
+
+  export default {
+    components: {
+      HeaderComponent,
+      FooterComponent
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
