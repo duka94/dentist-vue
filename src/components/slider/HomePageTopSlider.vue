c7b87219034fcd7d1a5c338aede423116c2539ed
Milan <miliup@live.com>
2019-02-11 23:21:00 +0100

    in progress

--- src/components/slider/HomePageTopSlider.vue
+++ src/components/slider/HomePageTopSlider.vue
@@ -1,51 +1,11 @@
 <template>
     <div>
-        <carousel :per-page="1" :paginationActiveColor="'#00aeb0'" :autoplay="true" :autoplayTimeout="1500" :loop="true">
-            <slide class="slide_img slide_1">
+        <carousel :per-page="1" :paginationActiveColor="'#00aeb0'" :autoplay="false" :autoplayTimeout="1500" :loop="true">
+            <slide v-for="image in images" :key="image.text" :style="{ 'background-image': 'url('+image.url+')'} ">
                 <div class="slide__content">
                     <div class="slide__content-header">
-                        <h1>Ordinacija</h1>
-                        <p class="slide__content-header-p">Prijemni deo</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="slide_img slide_2">
-                <div class="slide__content">
-                    <div class="slide__content-header">
-                        <h1>Ordinacija</h1>
-                        <p class="slide__content-header-p">Sala broj 1</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="slide_img slide_3">
-                <div class="slide__content">
-                    <div class="slide__content-header">
-                        <h1>Ordinacija</h1>
-                        <p class="slide__content-header-p">Sala broj 2</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="slide_img slide_4">
-                <div class="slide__content">
-                    <div class="slide__content-header">
-                        <h1>Ordinacija</h1>
-                        <p class="slide__content-header-p">Sala broj 3</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="slide_img slide_5">
-                <div class="slide__content">
-                    <div class="slide__content-header">
-                        <h1>Toalet</h1>
-                        <p class="slide__content-header-p">Za pacijente</p>
-                    </div>
-                </div>
-            </slide>
-            <slide class="slide_img slide_6">
-                <div class="slide__content">
-                    <div class="slide__content-header">
-                        <h1>Lekarska</h1>
-                        <p class="slide__content-header-p">Soba</p>
+                        <h1> {{ image.h1 }} </h1>
+                        <p class="slide__content-header-p">{{ image.text }}</p>
                     </div>
                 </div>
             </slide>
@@ -63,32 +23,32 @@
                     {
                         h1: 'Ordinacija',
                         text: 'Prijemni deo',
-                        url: '../../images/home/top-slider/ordinacija_1.jpeg',
+                        url: '../../images/home/top-slider/prijemni_deo.jpeg',
                     },
                     {
                         h1:'Ordinacija',
                         text: 'Sala broj 1',
-                        url: 'http://www.sreckov.com/storage/uploads/homeslideshow/b4ea6af70cb7edf2225701136aab6608.jpeg',
+                        url: '../../images/home/top-slider/ordinacija_1.jpeg',
                     },
                     {
                         h1: 'Ordinacija',
                         text: 'Sala broj 2',
-                        url: 'http://www.sreckov.com/storage/uploads/homeslideshow/695576534bb8c9a23feb0175a7a23707.jpeg',
+                        url: '../../images/home/top-slider/ordinacija_2.jpeg',
                     },
                     {
                         h1: 'Ordinacija',
                         text: 'Sala broj 3',
-                        url: 'http://www.sreckov.com/storage/uploads/homeslideshow/d9c64388ec4de071b9ebf9b16d4f58ec.jpeg',
+                        url: '../../images/home/top-slider/ordinacija_3.jpeg',
                     },
                     {
                         h1: 'Toalet',
                         text: 'Za pacijente',
-                        url: 'http://www.sreckov.com/storage/uploads/homeslideshow/8554c9c392d029b3dc0c7c6013f35a85.jpeg',
+                        url: '../../images/home/top-slider/toalet.jpeg',
                     },
                     {
                         h1: 'Lekarska',
                         text: 'Soba',
-                        url: 'http://www.sreckov.com/storage/uploads/homeslideshow/42b22877ccba024982751f4b2012f366.jpeg',
+                        url: '../../images/home/top-slider/lekarska_soba.jpeg',
                     },
                 ]
             }
@@ -99,25 +59,3 @@
         }
     }
 </script>
-
-<style scoped lang="scss">
-    .slide_1 {
-        background: url('../../images/home/top-slider/prijemni_deo.jpeg');
-    }
-    .slide_2 {
-        background: url('../../images/home/top-slider/ordinacija_1.jpeg');
-    }
-    .slide_3 {
-        background: url('../../images/home/top-slider/ordinacija_2.jpeg');
-    }
-    .slide_4 {
-        background: url('../../images/home/top-slider/ordinacija_3.jpeg');
-    }
-    .slide_5 {
-        background: url('../../images/home/top-slider/toalet.jpeg');
-    }
-    .slide_6 {
-        background: url('../../images/home/top-slider/lekarska_soba.jpeg');
-    }
-
-</style>
