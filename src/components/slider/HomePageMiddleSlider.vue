a6237a15eab6d62d9a5786720660a6d59c4ee780
Milan <miliup@live.com>
2019-02-26 13:49:25 +0100

    corrected code and added scroll page for slide

--- src/components/slider/HomePageMiddleSlider.vue
+++ src/components/slider/HomePageMiddleSlider.vue
@@ -3,20 +3,20 @@
         <header>
             <h1>Upoznajte naš tim</h1>
         </header>
-        <carousel class="container"
+        <carousel
+                  class="container"
                   per-page="4"
                   :autoplay="false"
                   :navigationEnabled="true"
                   :paginationEnabled="false"
                   :loop="true"
-                  navigationNextLabel="
-                  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512'><path fill='#00aeb0' path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'/></svg>"
-                  navigationPrevLabel="
-                  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512'><path fill='#00aeb0' path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'/></svg>"
+                  :scrollPerPage="false"
+                  navigationNextLabel="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512'><path fill='#00aeb0' path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'/></svg>"
+                  navigationPrevLabel="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512'><path fill='#00aeb0' path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'/></svg>"
         >
             <slide v-for="image in images" :key="image.p">
-                <div class="slide__content">
-                    <img v-bind:src="image.image">
+                <div class="slide__content-middle">
+                    <img class="" v-bind:src="image.image">
                     <p> {{ image.p }} </p>
                 </div>
             </slide>
