51356159e9a6b7a1e222af5ec1a4684387d30844
Zeljka Unkovic <zeljka.u@outlook.com>
2019-02-28 20:17:31 +0100

    Fix style header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,33 +1,29 @@
 <template>
   <section class="section">
-    <header>
-      <div class="container flex">
+    <header class="header-section container">
+      <div class="row">
         <div class="col-md-4">
-          <figure>
-            <a href="/">
-              <div class="logo">
-                <img src="../../images/logo/logo.png">
-              </div>
-            </a>
+          <figure class="logo">
+            <a href="/"> <img src="../../images/logo/logo.png"></a>
           </figure>
         </div>
         <div class="col-md-8">
           <nav class="flex w-100">
             <ul class="w-100">
               <li class="nav-item active">
-                <a class="link" href="/"> POCETNA</a>
+                <a class="link" href="/">početna</a>
               </li>
               <li class="nav-item">
-                <a class="link" href="#"> ZAKAZIVANJE</a>
+                <a class="link" href="#"> zakazivanje</a>
               </li>
               <li class="nav-item">
-                <a class="link" href="#"> O NAMA</a
+                <a class="link" href="#"> o nama</a
                 ></li>
               <li class="nav-item">
-                <a class="link" href="#">USLUGE</a>
+                <a class="link" href="#">usluge</a>
               </li>
               <li class="nav-item">
-                <a class="link" href="#"> GALERIJA</a>
+                <a class="link" href="#"> galerija</a>
               </li>
               <li class="nav-item">
                 <a class="link" href="#">KONTAKT</a>
